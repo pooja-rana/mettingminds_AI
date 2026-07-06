5f9aede68e344bb2b3a10a3e184881f903b432f0
Pooja IRVX <poojairvx@Poojas-MacBook-Air.local>
2026-07-06 18:32:23 +0530

    add logger functionality

--- app/repositories/meeting_repository.py
+++ app/repositories/meeting_repository.py
@@ -1,22 +1,33 @@
+import logging
+
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.models.meeting import Meeting
 
+logger = logging.getLogger(__name__)
+
 
 class MeetingRepository:
     def __init__(self, session: Session):
         self.session = session
+        logger.debug("MeetingRepository initialized")
 
     def create(self, meeting: Meeting) -> Meeting:
+        logger.info("Persisting meeting: %s", meeting.title)
         self.session.add(meeting)
         self.session.commit()
         self.session.refresh(meeting)
+        logger.info("Persisted meeting id=%s", meeting.id)
         return meeting
 
     def get_all(self) -> list[Meeting]:
+        logger.info("Querying all meetings")
         statement = select(Meeting).order_by(Meeting.created_at.desc())
-        return self.session.scalars(statement).all()
+        meetings = self.session.scalars(statement).all()
+        logger.info("Retrieved %s meetings", len(meetings))
+        return meetings
 
     def get_by_id(self, meeting_id: int) -> Meeting | None:
+        logger.info("Querying meeting by id=%s", meeting_id)
         return self.session.get(Meeting, meeting_id)
